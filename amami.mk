6dee9330657d7236b323c690b928f6c1c42002c3
Juan Gomez <jgomez@mozilla.com>
2016-03-10 13:29:56 +0100

    Bug 1087161 - Use gcc-4.9 toolchain to build Gecko r=gerard-majax

--- amami.mk
+++ amami.mk
@@ -24,4 +24,7 @@ BOOTANIMATION_ASSET_SIZE := 720p
 PRODUCT_COPY_FILES += \
     device/sony/amami/bootrec-device:root/sbin/bootrec-device
 
+# Set Gecko toolchain
+GECKO_TOOLS_PREFIX = prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+
 $(call inherit-product-if-exists, vendor/sony/amami-blobs/amami-vendor-blobs.mk)
